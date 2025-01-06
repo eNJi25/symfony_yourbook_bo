f995370ac8e37bcf5df3754e7256cef14f9dbd63
eNJi25 <nfischer.contact@gmail.com>
2025-01-06 20:41:07 +0100

    feat : upload image Livre

--- src/Controller/Admin/LivreCrudController.php
+++ src/Controller/Admin/LivreCrudController.php
@@ -5,9 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\Livre;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class LivreCrudController extends AbstractCrudController
 {
@@ -18,7 +21,12 @@ class LivreCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield from parent::configureFields($pageName);
+        // yield from parent::configureFields($pageName);
+        yield TextField::new('titre');
+        yield TextField::new('isbn');
+        yield TextareaField::new('description');
+        yield BooleanField::new('archive');
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class);
         yield AssociationField::new('auteur');
         yield AssociationField::new('editeur');
         yield AssociationField::new('genres');
